26ed3208f815fcd65dd0e7de8c4a765c8c00ea46
Balaji-Sohan <balajiaddepalli88@gmail.com>
2024-11-23 11:36:37 +0530

    change

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 // script.js
 
-// Function to select two random images
-function getRandomImages(arr, num) {
-  const shuffled = arr.slice().sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, num);
+// Function to select one random image
+function getRandomImage(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
 
 // Fetch the list of images from images.json
 fetch('images.json')
   .then(response => response.json())
   .then(imageFilenames => {
-    // Display the images
+    // Display the image
     const cartoonContainer = document.getElementById('cartoon-container');
-    const dailyImages = getRandomImages(imageFilenames, 1);
+    const filename = getRandomImage(imageFilenames);
 
-    dailyImages.forEach((filename) => {
-      const img = document.createElement('img');
-      const encodedFilename = encodeURIComponent(filename);
-      img.src = 'images/' + encodedFilename;
-      img.className = 'cartoon';
-      cartoonContainer.appendChild(img);
-    });
+    const img = document.createElement('img');
+    const encodedFilename = encodeURIComponent(filename);
+    img.src = 'images/' + encodedFilename;
+    img.className = 'cartoon';
+    cartoonContainer.appendChild(img);
   })
   .catch(error => {
     console.error('Error fetching images:', error);
     const cartoonContainer = document.getElementById('cartoon-container');
-    cartoonContainer.innerHTML = '<p>Error loading images. Please try again later.</p>';
+    cartoonContainer.innerHTML = '<p>Error loading image. Please try again later.</p>';
   });
+
